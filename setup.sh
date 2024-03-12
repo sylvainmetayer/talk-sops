75f7fd4db8667495c00f6fcff749ce0eb1169158
Sylvain <s.metayer@groupeonepoint.com>
2024-03-12 15:22:25 +0100

    clean

--- setup.sh
+++ setup.sh
@@ -5,5 +5,4 @@ curl -sLo code/demo-magic.sh https://raw.githubusercontent.com/paxtonhare/demo-m
 git clone https://github.com/hakimel/reveal.js --depth 1 --branch 5.0.5
 git clone https://github.com/denehyg/reveal.js-menu --depth 1 --branch 2.1.0 reveal.js/plugin/menu
 cd reveal.js && npm ci && npm run build && cd ../
-curl -sL 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css' -o css/font-awesome.css
 curl -sLo js/hightlight.min.js https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.7.3/highlight.min.js
