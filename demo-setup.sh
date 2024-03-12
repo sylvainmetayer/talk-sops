c86e0a11831e23912cda80bb1ec369e2970588ad
Sylvain <s.metayer@groupeonepoint.com>
2024-03-12 14:10:40 +0100

    add timer

--- demo-setup.sh
+++ demo-setup.sh
@@ -2,3 +2,13 @@
 
 gnome-terminal --window-with-profile=demo --full-screen --working-directory "$(pwd)/code"
 firefox -P work --new-window 'http://localhost:8000'
+
+echo "Silent mode is on?"
+
+echo "Battery saver"
+sudo systemctl stop docker.service
+sudo systemctl stop cbagentd.service
+sudo systemctl stop forticlient.service
+
+
+kubectx -u || true
