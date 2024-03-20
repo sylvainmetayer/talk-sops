c4e60f363cae8969aa240da6ee27006093cae0dd
Sylvain <s.metayer@groupeonepoint.com>
2024-03-20 10:21:41 +0100

    feat: add slides link

--- demo-setup.sh
+++ demo-setup.sh
@@ -3,11 +3,13 @@
 echo "Silent mode is on?"
 
 echo "Battery saver"
-sudo systemctl stop docker.service
-sudo systemctl stop cbagentd.service
-sudo systemctl stop forticlient.service
+#sudo systemctl stop docker.service
+#sudo systemctl stop cbagentd.service
+#sudo systemctl stop forticlient.service
 
 kubectx -u || true
 
 gnome-terminal --window-with-profile=demo --full-screen --working-directory "$(pwd)/code"
-firefox -P work --new-window 'http://localhost:8000'
+# Update with current demo
+firefox -P work 'https://sops.talks.sylvain.dev/' 'http://localhost:8000'
+systemctl restart --user xremap
--- setup.sh
+++ setup.sh
@@ -6,3 +6,7 @@ git clone https://github.com/hakimel/reveal.js --depth 1 --branch 5.0.5
 git clone https://github.com/denehyg/reveal.js-menu --depth 1 --branch 2.1.0 reveal.js/plugin/menu
 cd reveal.js && npm ci && npm run build && cd ../
 curl -sLo js/hightlight.min.js https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.7.3/highlight.min.js
+
+#qrencode https://openfeedback.io/o7zyKdkdRulRnrtU3DZo/2024-02-02/6b97ee2f37ba238fa00f0ff7a02b6049 -o images/openfeedback.svg -t SVG -l H -m 2
+# TODO For DevoxxFR, update with tagged version
+qrencode https://sops.talks.sylvain.dev/ -o images/slides_link.svg -t SVG -l H -m 2
