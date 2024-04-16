da74395f5031d34670ba87d8cd7f68de82957e49
Sylvain <s.metayer@groupeonepoint.com>
2024-04-16 13:40:40 +0200

    update qrcodes

--- setup.sh
+++ setup.sh
@@ -9,4 +9,4 @@ curl -sLo js/hightlight.min.js https://cdnjs.cloudflare.com/ajax/libs/highlight.
 
 #qrencode https://app.voxxr.in/events/devoxxfr2024/rate-talk/21890 -o images/openfeedback.svg -t SVG -l H -m 2
 # TODO For DevoxxFR, update with tagged version
-#qrencode https://sops.talks.sylvain.dev/ -o images/slides_link.svg -t SVG -l H -m 2
+#qrencode http://r.sylvain.dev/devoxxfr-2024-sops -o images/slides_link.svg -t SVG -l H -m 2
