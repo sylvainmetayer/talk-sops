41cc80b209dda2a85467a02bfe5ae5b88c8374ca
Sylvain <s.metayer@groupeonepoint.com>
2024-06-24 13:50:38 +0200

    feat: breizhcamp

---
+++ qrCodes.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+qrencode https://openfeedback.io/LyIREj0UbxmZ6vcFmxmN/2024-06-27/652393 -o images/openfeedback.svg -t SVG -l H -m 2
+qrencode https://r.sylvain.dev/breizhcamp-2024-sops -o images/slides_link.svg -t SVG -l H -m 2
+qrencode https://ecole.groupeonepoint.com/home-formations-ecole-onepoint -o images/ecole_onepoint.svg -t SVG -l H -m 2
