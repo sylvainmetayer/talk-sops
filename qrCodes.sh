d2c28032e78ef88be8cd375aef2552b450f7c0e5
Sylvain <s.metayer@groupeonepoint.com>
2024-07-02 14:07:33 +0200

    feat: sunnytech

--- qrCodes.sh
+++ qrCodes.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-qrencode https://openfeedback.io/LyIREj0UbxmZ6vcFmxmN/2024-06-27/652393 -o images/openfeedback.svg -t SVG -l H -m 2
-qrencode https://r.sylvain.dev/breizhcamp-2024-sops -o images/slides_link.svg -t SVG -l H -m 2
+qrencode https://openfeedback.io/sunnytech2024/2024-07-05/sops-passez-un-savon-a-vos-sec -o images/openfeedback.svg -t SVG -l H -m 2
+qrencode https://r.sylvain.dev/sunnytech-2024-sops -o images/slides_link.svg -t SVG -l H -m 2
 qrencode https://ecole.groupeonepoint.com/home-formations-ecole-onepoint -o images/ecole_onepoint.svg -t SVG -l H -m 2
