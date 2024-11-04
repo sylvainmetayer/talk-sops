a3116cec5b99b46ea9f4f16e04c478de304f8928
Sylvain <s.metayer@groupeonepoint.com>
2024-11-04 09:56:32 +0100

    feat: devfest toulouse 2024

--- qrCodes.sh
+++ qrCodes.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-qrencode https://openfeedback.io/sunnytech2024/2024-07-05/sops-passez-un-savon-a-vos-sec -o images/openfeedback.svg -t SVG -l H -m 2
-qrencode https://r.sylvain.dev/sunnytech-2024-sops -o images/slides_link.svg -t SVG -l H -m 2
+qrencode https://openfeedback.io/devfest-toulouse-2024/2024-11-07/681338 -o images/openfeedback.svg -t SVG -l H -m 2
+qrencode https://r.sylvain.dev/devfest-toulouse-2024 -o images/slides_link.svg -t SVG -l H -m 2
 qrencode https://ecole.groupeonepoint.com/home-formations-ecole-onepoint -o images/ecole_onepoint.svg -t SVG -l H -m 2
+qrencode https://forms.office.com/e/VXH9uj0hf6 -o images/prize.svg -t SVG -l H -m 2
