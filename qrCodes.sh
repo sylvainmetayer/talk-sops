3a488ea5d926f0a1e94490b2a2034cff998de10a
Sylvain <s.metayer@groupeonepoint.com>
2025-01-13 14:05:01 +0100

    feat: prepare snowcamp

--- qrCodes.sh
+++ qrCodes.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-qrencode https://openfeedback.io/devfest-toulouse-2024/2024-11-07/681338 -o images/openfeedback.svg -t SVG -l H -m 2
-qrencode https://r.sylvain.dev/devfest-toulouse-2024 -o images/slides_link.svg -t SVG -l H -m 2
+qrencode https://app.voxxr.in/events/snowcamp25/rate-talk/cm28wxftc00da6gvvj6kngs89 -o images/openfeedback.svg -t SVG -l H -m 2
+qrencode https://r.sylvain.dev/snowcamp-2025 -o images/slides_link.svg -t SVG -l H -m 2
 qrencode https://ecole.groupeonepoint.com/home-formations-ecole-onepoint -o images/ecole_onepoint.svg -t SVG -l H -m 2
-qrencode https://forms.office.com/e/VXH9uj0hf6 -o images/prize.svg -t SVG -l H -m 2
