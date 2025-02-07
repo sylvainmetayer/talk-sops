c9df7344c8ca530d378d615ef7c2015969459caa
Sylvain <s.metayer@groupeonepoint.com>
2025-02-07 11:44:47 +0100

    feat: add openfeedback

--- qrCodes.sh
+++ qrCodes.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-qrencode https://app.voxxr.in/events/snowcamp25/rate-talk/cm28wxftc00da6gvvj6kngs89 -o images/openfeedback.svg -t SVG -l H -m 2
+qrencode https://openfeedback.io/w3JorGi7ut6d9keUsCvO/2025-02-07/cm09o1gh4000hpogqepwja8kg -o images/openfeedback.svg -t SVG -l H -m 2
 qrencode https://r.sylvain.dev/sops-tnt-2025 -o images/slides_link.svg -t SVG -l H -m 2
