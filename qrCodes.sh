bd87862bea95410f871e7c2ab7c1dd82d1703d21
Sylvain <s.metayer@groupeonepoint.com>
2025-02-07 09:40:33 +0100

    feat: tnt

--- qrCodes.sh
+++ qrCodes.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 qrencode https://app.voxxr.in/events/snowcamp25/rate-talk/cm28wxftc00da6gvvj6kngs89 -o images/openfeedback.svg -t SVG -l H -m 2
-qrencode https://r.sylvain.dev/snowcamp-2025 -o images/slides_link.svg -t SVG -l H -m 2
-qrencode https://ecole.groupeonepoint.com/home-formations-ecole-onepoint -o images/ecole_onepoint.svg -t SVG -l H -m 2
+qrencode https://r.sylvain.dev/sops-tnt-2025 -o images/slides_link.svg -t SVG -l H -m 2
--- setup.sh
+++ setup.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-
 curl -sLo code/demo-magic.sh https://raw.githubusercontent.com/paxtonhare/demo-magic/master/demo-magic.sh
 git clone https://github.com/hakimel/reveal.js --depth 1 --branch 5.0.5
 git clone https://github.com/denehyg/reveal.js-menu --depth 1 --branch 2.1.0 reveal.js/plugin/menu
 cd reveal.js && npm ci && npm run build && cd ../
 git clone https://github.com/highlightjs/highlight.js --depth=1 -b 11.9.0
-cd highlight.js && npm ci && npm run build-cdn && cp -r build/highlight.min.js build/styles ../js/highlight
+cd highlight.js && npm ci && npm run build-cdn && cp -r build/highlight.min.js build/styles build/languages build/es ../js/highlight
