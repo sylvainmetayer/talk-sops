083c35f6a1f414e6434ec670f0c8f430643f7f76
Sylvain <s.metayer@groupeonepoint.com>
2025-10-27 11:22:36 +0100

    feat: podman

--- qrCodes.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-qrencode https://openfeedback.io/w3JorGi7ut6d9keUsCvO/2025-02-07/cm09o1gh4000hpogqepwja8kg -o images/openfeedback.svg -t SVG -l H -m 2
-qrencode https://r.sylvain.dev/sops-tnt-2025 -o images/slides_link.svg -t SVG -l H -m 2
--- run.sh
+++ run.sh
@@ -5,6 +5,6 @@
 # https://github.com/asciidoctor/asciidoctor-reveal.js/issues/248#issuecomment-579277817
 while inotifywait -e close_write index.adoc slides js images diagrams css index-docinfo-footer.html; do
   echo -n "Detected changes, rebuild presentation... "
-  docker compose up
+  podman-compose up
   echo "Done !"
 done
